c40dfa47cf166b72b758b58624ccd6f56bacee40
Muhammad Usman <alidevelopement01@gmail.com>
2026-02-19 23:39:26 +0500

    chages

--- database/migrations/2021_08_02_120956_create_services_table.php
+++ database/migrations/2021_08_02_120956_create_services_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
+            // $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->double('charges')->nullable();
             $table->boolean('status')->default(1);
             $table->timestamps();
 
-            $table->foreign('category_id')->references('id')->on('service_categories')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
+            // $table->foreign('category_id')->references('id')->on('service_categories')
+            //     ->onUpdate('cascade')
+            //     ->onDelete('cascade');
         });
     }
 
--- database/migrations/2026_01_02_215542_add_duration_to_services.php
+++ database/migrations/2026_01_02_215542_add_duration_to_services.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::table('services', function (Blueprint $table) {
             $table->integer('duration')->default(0);
             // if (Schema::hasColumn('services', 'category_id')) {
-                $table->dropForeign(['category_id']);
-                $table->dropColumn('category_id');
+                // $table->dropForeign(['category_id']);
+                // $table->dropColumn('category_id');
             // }
             $table->string('category_id')->nullable();
         });
